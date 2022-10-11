da9691ffd6231e2a38722eb5717d23be23c84c9f
jordan <jharrison1@qa.com>
2022-10-11 14:12:53 +0100

    added testing

---
+++ tests/maths.test.js
@@ -0,0 +1,18 @@
+const {sum} = require("../maths");
+const { expect } = require("chai");
+
+describe("maths tests", () => { // set of tests
+    it("should equal 2", () => { // single test
+        expect(sum(1, 1)).to.equal(2);
+
+        // expect:
+        // if (sum(1,1) === 2) {
+        //     pass();
+        // } else {
+        //     fail();
+        // }
+     })
+     it("should fail", () => {
+        expect(sum(1, 1)).to.equal(3);
+     })
+});
